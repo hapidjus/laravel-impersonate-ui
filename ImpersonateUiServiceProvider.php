c2873bcf513554cb165f9c0798bb51a0944c3bf5
hapidjus <ola.ebbesson@caesardev.se>
2019-10-11 23:30:37 +0200

    Update ImpersonateUiServiceProvider.php

--- ImpersonateUiServiceProvider.php
+++ ImpersonateUiServiceProvider.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Riak\Connection;
+
+class ImpersonateUiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        return true;
+    }
+}
