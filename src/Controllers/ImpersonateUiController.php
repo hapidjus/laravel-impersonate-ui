30c20db65549d826c9032996d42756450bfc13b2
hapidjus <ola.ebbesson@caesardev.se>
2019-10-14 00:01:07 +0200

    Create ImpersonateUiController.php

---
+++ src/Controllers/ImpersonateUiController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Hapidjus\ImpersonateUI\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Auth\AuthManager;
+use App\User;
+use Auth;
+
+class ImpersonateUiController extends Controller
+{
+
+	private $manager;
+
+	public function __construct(){
+
+		$this->manager = app('impersonate');
+
+	}
+
+    public function take(Request $request)
+    {
+
+    	
+    	if($this->manager->isImpersonating())
+    	{
+    		$this->manager->leave();
+    	}
+
+    	$this->manager->take(Auth::user(), $this->manager->findUserById($request->impersonate_id));
+
+    	return redirect()->back();
+
+    }
+
+    public function leave(Request $request)
+    {
+
+		if($this->manager->isImpersonating())
+		{
+			$manager->leave();
+		}    	
+
+    	return redirect()->back();
+
+    }
+
+
+}
