de357ce24ca26e6f77959577bdb791033f60ebde
hapidjus <ola.ebbesson@caesardev.se>
2019-10-14 00:01:57 +0200

    Update ImpersonateUiServiceProvider.php

--- src/ImpersonateUiServiceProvider.php
+++ src/ImpersonateUiServiceProvider.php
@@ -4,6 +4,8 @@ namespace Hapidjus\ImpersonateUI;
 
 use Illuminate\Support\ServiceProvider;
 use Riak\Connection;
+use Hapidjus\ImpersonateUI\Middlewares\ImpersonateUiMiddleware;
+
 
 class ImpersonateUiServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,22 @@ class ImpersonateUiServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        return true;
+
+        app('Illuminate\Contracts\Http\Kernel')->pushMiddleware(ImpersonateUiMiddleware::class);
+        
+        $this->registerViews();
+
+        $this->registerRoutes();
+
+    }
+
+    protected function registerViews()
+    {
+    	$this->loadViewsFrom(__DIR__ . '/../resources/views', 'impersonate-ui');
+    }
+
+    protected function registerRoutes()
+    {
+		$this->loadRoutesFrom(__DIR__.'/Routes/ImpersonateUiRoutes.php');
     }
 }
