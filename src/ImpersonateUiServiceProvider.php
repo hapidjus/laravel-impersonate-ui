ecce780ae1743e1d5dce33bd208f6713f1cb22b5
hapidjus <5553121+hapidjus@users.noreply.github.com>
2019-10-16 18:27:23 +0200

    Add register config and View composer

--- src/ImpersonateUiServiceProvider.php
+++ src/ImpersonateUiServiceProvider.php
@@ -5,10 +5,25 @@ namespace Hapidjus\ImpersonateUI;
 use Illuminate\Support\ServiceProvider;
 use Riak\Connection;
 use Hapidjus\ImpersonateUI\Middlewares\ImpersonateUiMiddleware;
+use Illuminate\Support\Facades\View;
+use App\User;
+
 
 
 class ImpersonateUiServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/laravel-impersonate-ui.php' => config_path('laravel-impersonate-ui.php'),
+        ]);
+    }
+
     /**
      * Register bindings in the container.
      *
@@ -23,15 +38,43 @@ class ImpersonateUiServiceProvider extends ServiceProvider
 
         $this->registerRoutes();
 
+        $this->mergeConfig();
+
     }
 
     protected function registerViews()
     {
     	$this->loadViewsFrom(__DIR__ . '/../resources/views', 'impersonate-ui');
+        View::composer('impersonate-ui::impersonate-ui', function ($view) {
+            $view->with([
+                'impersonator'  => $this->getImpersonator(),
+                'users'         => User::orderBy('name')->get()
+            ]);
+        });
     }
 
     protected function registerRoutes()
     {
 		$this->loadRoutesFrom(__DIR__.'/Routes/ImpersonateUiRoutes.php');
     }
+
+    protected function mergeConfig()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-impersonate-ui.php', 'laravel-impersonate-ui');
+    }
+
+    public function getImpersonator(){
+
+        $manager = app('impersonate');
+
+        if($manager->getImpersonatorId() !== null)
+        {
+            return User::findOrFail($manager->getImpersonatorId());
+
+        }
+
+        return null;
+
+    }
+
 }
